d6cf73cc08e336e92dc06ce9ee4b0fdc719163bd
53able <gt_vengeance@yahoo.co.jp>
2023-06-03 01:13:15 +0900

    要約したテキストを .md ファイルに保存できるようになった

--- package/main.py
+++ package/main.py
@@ -2,13 +2,13 @@
 
 import argparse
 import glob
-import hashlib
 import os
 import re
 import shutil
 import tkinter as tk
 from concurrent.futures import ThreadPoolExecutor
 from tkinter import filedialog
+import time
 
 import ffmpeg
 import openai
@@ -17,9 +17,11 @@ from pytube import YouTube
 MODEL = "gpt-3.5-turbo"
 TMP_PATH = "tmp"
 
+# 一時ファイル命名用のユニックスタイムスタンプ
+TIMESTAMP = str(int(time.time()))
 
-# 一時的なディレクトリを作成
-# 既に存在する場合は削除してから新しく作成
+
+# 一時ファイルを作成
 def make_tmp_dir():
     if os.path.exists(TMP_PATH):
         shutil.rmtree(TMP_PATH)
@@ -39,21 +41,15 @@ def validate_youtube_url(url):
     return bool(youtube_pattern.match(url))
 
 
-# 指定されたURLのYouTubeのビデオをダウンロード
-# ビデオのタイトルをSHA256でハッシュ化してファイル名に使用
+# YouTubeのビデオをダウンロード
 def download_youtube_video(url, output_path):
     yt = YouTube(url)
 
-    hash_object = hashlib.sha256(yt.title.encode())
-    hashed_title = hash_object.hexdigest()
-
     yt.streams.filter(progressive=True, file_extension='mp4').order_by(
-        'resolution').asc().first().download(output_path, f"{hashed_title}.mp4")
-    return hashed_title
+        'resolution').asc().first().download(output_path, f"{TIMESTAMP}.mp4")
 
 
-# 入力ファイルの音声を分割
-# 分割後のファイルはTMP_PATHに保存
+# 音声ファイルを指定された長さで分割
 def split_audio(input_file, duration, output_format, title="output"):
 
     shutil.rmtree(f"{TMP_PATH}/{title}.split.*.mp3", ignore_errors=True)
@@ -82,8 +78,7 @@ def transcribe_audio(audio_path):
         print(f"An unexpected error occurred: {e}")
 
 
-# テキストを指定された文字数で分割
-# 戻り地はテキストのリスト
+# 指定された文字数でテキストを分割
 def split_text(text, limit=2000):
     text_list = []
     while len(text) > limit:
@@ -120,23 +115,19 @@ def parallel_summarize_text(text_list, prompt):
             summary_text += result
     return summary_text
 
+generated_text = []
 
-# テキストを反復的に要約
-# 各イテレーションでテキストを分割し、並行して要約を実行
+# テキストを指定された文字数で分割しながら要約
 def parallel_iterative_summary(text, prompt, token_limit=2000):
     depth = 0
     summary = ""
 
     while True:
         text_list = split_text(text, token_limit)
-
         summary = parallel_summarize_text(text_list, prompt)
-
-        print(f"\n\n【要約 {depth}】:\n{summary}")
-
+        generated_text.append(f"## 要約 {depth}\n{summary}")
         if len(summary) <= token_limit:
             break
-
         text = summary
         depth += 1
 
@@ -150,26 +141,37 @@ def get_local_video_file():
     video_path = filedialog.askopenfilename()
     return video_path
 
+# テキストリストをファイルに保存
+def save_text_list(text_list):
+    # 並びを逆順にする
+    text_list.reverse()
 
-# ビデオファイルを処理し、その要約を作成
-# ビデオから音声を抽出し、音声をテキストに変換（トランスクリプト化）し、テキストを要約
+    root = tk.Tk()
+    root.withdraw()
+    file_path = filedialog.asksaveasfilename(
+        initialdir=TMP_PATH,
+        initialfile=TIMESTAMP,
+        defaultextension=".md",
+        filetypes=[("Markdown", ".md")]
+    )
+    with open(file_path, "w") as f:
+        f.write("\n\n".join(text_list))
+
+# ビデオファイルを処理
 def process_video(video_path):
 
-    hash_object = hashlib.sha256(video_path.encode())
-    video_title = hash_object.hexdigest()
-
     stream = ffmpeg.input(video_path)
-    audio_path = os.path.join(TMP_PATH, video_title + ".mp3")
+    audio_path = os.path.join(TMP_PATH, TIMESTAMP + ".mp3")
     stream = ffmpeg.output(stream, audio_path)
     ffmpeg.run(stream)
 
-    splits = split_audio(audio_path, 1200, ".mp3", video_title)
+    splits = split_audio(audio_path, 1200, ".mp3", TIMESTAMP)
 
     transcript_text_sum = ""
     with ThreadPoolExecutor() as executor:
         results = executor.map(transcribe_audio, splits)
         for index, result in enumerate(results):
-            print(f"Transcript {index}:\n{result}")
+            generated_text.append(f"## トランスクリプト {index}\n{result}")
             transcript_text_sum += result
 
     summary = parallel_iterative_summary(transcript_text_sum, "Please summarize the following sentences in Japanese, separating them into paragraphs and line breaks. Adjust the text to be natural. Please sort out redundant wording.:\n\n")
@@ -184,8 +186,8 @@ def summarize_video_from_youtube(video_url):
     if not validate_youtube_url(video_url):
         raise ValueError(f"'{video_url}' is not a valid YouTube URL.")
 
-    video_title = download_youtube_video(video_url, TMP_PATH)
-    video_path = os.path.join(TMP_PATH, f"{video_title}.mp4")
+    download_youtube_video(video_url, TMP_PATH)
+    video_path = os.path.join(TMP_PATH, f"{TIMESTAMP}.mp4")
 
     return process_video(video_path)
 
@@ -199,19 +201,28 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Summarize a video.')
     parser.add_argument('api_key', type=str, help='OpenAI API Key')
     parser.add_argument('video_url', type=str, nargs='?', default=None, help='Path to the video file')
+    parser.add_argument('--clean', action='store_true', help='Clean up temporary files')
     args = parser.parse_args()
+
+    # 一時ファイルを削除
+    if args.clean:
+        shutil.rmtree(TMP_PATH)
 
     # api_keyを設定
     openai.api_key = args.api_key
-
-    make_tmp_dir()
 
     if args.video_url is None:
         video_path = get_local_video_file()
         if video_path is None:
             raise ValueError("No video file was selected.")
-        summary = summarize_video(video_path)
+        summary_title = summarize_video(video_path)
     else:
-        summary = summarize_video_from_youtube(args.video_url)
+        summary_title = summarize_video_from_youtube(args.video_url)
 
-    print(f"\n\n【要約タイトル】:\n{summary}")
+    # 生成に失敗したら代替えのタイトルを設定
+    if summary_title is None:
+        summary_title = "タイトルが生成できませんでした。"
+
+    generated_text.append(f"# {summary_title}")
+
+    save_text_list(generated_text)
